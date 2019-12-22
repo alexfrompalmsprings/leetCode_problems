4f1208f2dfd1a1d27a5c64122a737a9e8768e8f7
AlexAvila <alexavila.oc@gmail.com>
2019-12-21 22:01:23 -0800

    SOLVED; intersectionTwoLinkedList.js

--- intersectionTwoLinkedLists.js
+++ intersectionTwoLinkedLists.js
@@ -3,14 +3,58 @@
  * function ListNode(val) {
  *     this.val = val;
  *     this.next = null;
+ *
+ *
+  * If the two linked lists have no intersection at all, return null.
+  The linked lists must retain their original structure after the function returns.
+  You may assume there are no cycles anywhere in the entire linked structure.
+  Your code should preferably run in O(n) time and use only O(1) memory.
+ *
  * }
  */
 
-/**
- * @param {ListNode} headA
- * @param {ListNode} headB
- * @return {ListNode}
- */
-var getIntersectionNode = function(headA, headB) {
+ // A little visual for us to use.
+
+// A:          a1 → a2
+//                    ↘
+//                      c1 → c2 → c3
+//                    ↗
+// B:     b1 → b2 → b3
+
+
+
+var getIntersectionNode = function (headA, headB) {
+
+  // first edge case: no heads
+  if(!headA || !headB){
+    return null;
+  }
+
+  // make a couple of pointers
+  var pointerOne = headA;
+  var pointerTwo = headB;
+
+  // check while we have the pointers and they're not the same
+  while(pointerOne && pointerTwo && pointerOne !== pointerTwo){
+    // increase them to the next node
+    pointerOne = pointerOne.next;
+    pointerTwo = pointerTwo.next;
+
+    // all the return statements that we are going to use
+    if(pointerOne === pointerTwo){
+      return pointerOne;
+    }
+
+    if(!pointerOne){
+      pointerOne = headB
+    }
+
+    if(!pointerTwo){
+      pointerTwo = headA
+    }
+
+  }
+  return pointerOne;
+
 
 };
