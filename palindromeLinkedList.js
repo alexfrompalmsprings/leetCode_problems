6d7e47b925ce8a619a2a7a844c955ce702feb530
AlexAvila <alexavila.oc@gmail.com>
2019-12-21 22:36:22 -0800

    add palindromeLinkedList.js

---
+++ palindromeLinkedList.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+// Input: 1->2->2->1
+// Output: true
+
+
+var isPalindrome = function(head) {
+
+
+};
