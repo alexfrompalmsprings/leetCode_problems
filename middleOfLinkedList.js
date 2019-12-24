eb6961dda51be937da6f2666555488abab06f23c
AlexAvila <alexavila.oc@gmail.com>
2019-12-24 12:10:03 -0800

    SOLVED middleOfLinkedList.js

--- middleOfLinkedList.js
+++ middleOfLinkedList.js
@@ -10,20 +10,30 @@
  * @return {ListNode}
  */
 
+/*
+! the method below is the best way to solve the problem:
+! two pointers: Slow pointer and a Fast pointer
+! the trick is right here -->
+!WHEN FAST REACHES THE END OF THE LIST
+! SLOW MUST BE IN THE IN THE MIDDLE
+*/
 
-var middleNode = function(head) {
-  var middle;
 
-  if(head.length % 2 === 1){
-    middle= Math.floor(head.length / 2)
-    console.log('this node is odd;', head[middle]);
-  } else {
-    middle = head.length / 2;
-    console.log('this node is even;', head[middle]);
+var middleNode = function (head) {
+  //our usual pointers
+  var fast = head;
+  var slow = head;
+
+  //create the loop to help our nodes move
+  while(fast && fast.next){
+      // assign them their speed
+      slow = slow.next;
+      fast = fast.next.next;
   }
-  return head.slice(middle)
-};
+  //return the slow since it reached the middle
+  return slow;
+}
 
-var example = [1,2,3,4,5]
+var example = [1, 2, 3, 4, 5]
 var test = middleNode(example);
 console.log(test);
