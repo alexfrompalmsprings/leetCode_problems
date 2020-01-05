d6405ab86dce35286090486fbce1ddee184600a9
AlexAvila <alexavila.oc@gmail.com>
2020-01-04 23:33:45 -0800

    comlete bubbleSortRecursive.js

--- leetCodes/bubbleSortRecursion.js
+++ leetCodes/bubbleSortRecursion.js
@@ -15,10 +15,51 @@
  */
 
 var bubbleSortRecursive = function (array) {
+  //create an inner var
+  var swapped;
 
+  for (var i = 0; i < array.length; i++) {
+    //declare the currents
+    var current = array[i];
+    var currentIdx = i;
 
+    //declare the nexts
+    var next = array[i + 1];
+    var nextIdx = i +1;
+
+    //conditional statement that runs while the current is greater than next
+    if (current > next) {
+      console.log('this the array at this moment',array)
+
+      //! SWAP MAGIC!!!
+
+      //create a temp var to swap
+      var temp = current;
+      // console.log('current ----->',current)
+      array[currentIdx] = next;
+      array[nextIdx] = temp;
+
+      // declare the swapped true
+      swapped = true
+    }
+  }
+
+  // !lets make the recursion happen!!
+  if(swapped === true){
+    bubbleSortRecursive(array)
+  } else {
+    console.log('the array was sorted bro!!')
+  }
+
+  //other wise we return it as it is
+  return array
+
+  // return swapped ? bubbleSortRecursive(array): array;
 
 };
 
 var test = bubbleSortRecursive([2, 1, 3, 6, 5, 4]);
+console.log('final', test) // yields [1, 2, 3, 4, 5, 6]
+
+var testSorted = bubbleSortRecursive([1, 2, 3, 4, 5, 6]);
 console.log('final', test) // yields [1, 2, 3, 4, 5, 6]
